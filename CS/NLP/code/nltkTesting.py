d949a86ce7de2cdedf6258945f1d54f7293e0142
minhduy105 <minhduy105@gmail.com>
2017-07-31 21:50:55 -0700

    add note for the next step

--- CS/NLP/code/nltkTesting.py
+++ CS/NLP/code/nltkTesting.py
@@ -55,3 +55,34 @@ try:
 
 except RuntimeError, e:
 	print "RuntimeError: ", e,
+
+
+
+# import sys
+
+# def read_until_minus_one():
+#     buf = []
+#     seen_minus = False
+#     while True:
+#         char = sys.stdin.read(1) 
+#         if not char: # EOF
+#             break 
+#         if char == '1' and seen_minus:
+#             buf.pop() # chop the last minus
+#             break # seen -1
+#         else:
+#             seen_minus = (char == '-')
+#             buf.append(char)
+#     return ''.join(buf)
+
+# print(read_until_minus_one())
+# #this is read letter, need to read character, add to the sentence, out put out for the text file
+
+
+# # Backspace is at codepoint 0008; delete is at 007F. (Notice how the image that you posted says "7F" in the text. That is the delete character.)
+
+# # Try this:
+
+# character = some_function_that_gets_a_character_from_stdin()
+# if character == '\x08' or character == '\x7f': 
+#   do_smth()
