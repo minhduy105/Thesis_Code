b256890542f6958f14ada5a458624044477e58dd
minhduy105 <minhduy105@gmail.com>
2017-11-08 02:35:21 -0800

    Finish correlation

--- Psych/RatingEmo/ReadingFile.py
+++ Psych/RatingEmo/ReadingFile.py
@@ -5,7 +5,8 @@ import csv
 
 def readInput(line,size,end):
     if len(line) > end:
-        if line[end-size:end] != '  ':
+        if line[end-size:end] != '  ' and line[end-size:end] != '\t\t':
+            print (line[end-size:end])
             return int(line[end-size:end])
     return -1000
 
@@ -14,7 +15,7 @@ def printResult(info,title,name):
     info.append(title)
     info.reverse()
     with open(name, "w") as t:
-        writer = csv.writer(t)
+        writer = csv.writer(t,lineterminator='\n')
         writer.writerows(info)
 
 np.set_printoptions(threshold=np.nan)
@@ -23,8 +24,15 @@ all = []
 for i in range(11):
     all.append([])
 col = []
-filename = "Introversion"
-title = ["Distant","Aloof","Unsociable","Introverted","Antisocial"]
+# filename = "Introversion"
+# title1 = ["Dyad","CoderGender","SorH","Conv","Distant","Aloof","Unsociable","Introverted","Antisocial"]
+# title2 = ["Distant","Aloof","Unsociable","Introverted","Antisocial"]
+
+filename = "Meek"
+title1 = ["Dyad","CoderGender","SorH","Conv","Timid","Yielding","Awkward","Shy","Meek"]
+title2 = ["Timid","Yielding","Awkward","Shy","Meek"]
+
+
 #read the data, ignore the self-rating in this case, delete or modify the if statement to get the data
 with open(filename+".txt") as f:
     for line in f:
@@ -59,7 +67,7 @@ for i in range (11):
 personTol = np.zeros((2,y,5))
 personCnt = np.zeros((2,y,5))
 for i in range (z):
-    if all[i][int(y/2)][3] == 2: #use y/2 to prevent input error
+    if all[i][int(y/2)][3] == 2: #use y/2 to prevent input error on H or S
         idz = 1
     else:
         idz = 0
@@ -71,20 +79,51 @@ for i in range (z):
 
 personAve = personTol/personCnt
 
+SAve = np.concatenate((all[0,:,1:5],personAve[0]),axis=1)
+HAve = np.concatenate((all[0,:,1:5],personAve[1]),axis=1)
+
 name = "Average\\"+filename+"_S.csv"
-printResult(personAve[0].tolist(),title,name)
+printResult(SAve.tolist(),title1,name)
 name = "Average\\"+filename+"_H.csv"
-printResult(personAve[1].tolist(),title,name)
-
-
-
-print (st.pearsonr ((personAve[1,:,0]),(personAve[1,:,1])))
+printResult(HAve.tolist(),title1,name)
 
 #calculate correlation and p
 # calculate correlation and p
 (z, y, x) = (personAve.shape)
+
+
 cor = np.zeros((z, x, x))
 pval = np.zeros((z, x, x))
-
 for i in range(z):
+    for k in range (x):
+        for j in range(x):
+            (peacor,pvalue) = st.pearsonr ((personAve[i,:,k]),(personAve[i,:,j]))
+            cor[i][k][j] = peacor
+            pval[i][k][j] = pvalue
+
+name = "Correlation\\"+filename+"_cor_S.csv"
+printResult(cor[0].tolist(),title2,name)
+name = "Correlation\\"+filename+"_cor_H.csv"
+printResult(cor[1].tolist(),title2,name)
+
+name = "Correlation\\"+filename+"_pval_S.csv"
+printResult(pval[0].tolist(),title2,name)
+name = "Correlation\\"+filename+"_pval_H.csv"
+printResult(pval[1].tolist(),title2,name)
+
+
+# combPersonAve = np.concatenate((personAve[0],personAve[1]),axis=0)
+# cor = np.zeros(( x, x))
+# pval = np.zeros(( x, x))
+#
+# for k in range (x):
+#     for j in range(x):
+#         (peacor,pvalue) = st.pearsonr ((combPersonAve[:,k]),(combPersonAve[:,j]))
+#         cor[k][j] = peacor
+#         pval[k][j] = pvalue
+# name = "Correlation\\"+filename+"_cor.csv"
+# printResult(cor.tolist(),title2,name)
+#
+# name = "Correlation\\"+filename+"_pval.csv"
+# printResult(pval.tolist(),title2,name)
 
